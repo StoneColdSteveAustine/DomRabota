ea5e43bbe70811c5d26f93e6fd925f7b71236341
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2022-11-29 21:14:52 +0600

    Update 66.cs

--- 66.cs
+++ 66.cs
@@ -1,1 +1,18 @@
+Console.WriteLine("Введите число M :");
+int m = int.Parse (Console.ReadLine());
 
+Console.WriteLine ("Введите число N :");
+int n = int.Parse(Console.ReadLine());
+
+void SumOfNumbers (int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine ($"Сумма натуральных элементов в промежутке от M до N - {sum}"); 
+        return;
+    }
+    sum = sum + (m++);
+    SumOfNumbers (m, n, sum);
+}
+
+SumOfNumbers(m, n, 0);
