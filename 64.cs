f3a2900673eb48e19ab61730f0502d43d775b856
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2022-11-29 21:13:57 +0600

    Update 64.cs

--- 64.cs
+++ 64.cs
@@ -1,1 +1,14 @@
+Console.WriteLine("Введите натуральное число :");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
 
+void CountDown (int n)
+{
+    if (n < 0)
+    Console.WriteLine($"{n} - ненатуральное число");
+    if (n == 0) 
+    return;
+    Console.Write(n);
+    CountDown (n - 1);
+}
+CountDown(n); 
